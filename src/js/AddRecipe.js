3a35062557344dd2327e102cc84d615197c182db
abazurek <skala2294@gmail.com>
2020-10-16 14:57:49 +0200

    form do add own recipe with no styling and no validate

--- src/js/AddRecipe.js
+++ src/js/AddRecipe.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function AddRecipe() {
     return(
-        <div>Dodaj</div>
+        <div className='container my-recipe'>
+            <h2>Dodaj władny przepis</h2>
+            <form>
+                <label> Nazwa dania
+                    <input type='text' placeholder='Wpisz nazwę dania'/>
+                </label>
+                <label> Potrzebne składniki
+                    <input type='text' placeholder='Wpisz potrzebne składniki po porzecinkach'/>
+                </label>
+                <label> Krótki opis dania
+                   <textarea placeholder='Wpisz krótki opis dania'/>
+                </label><label> Dokładny opis opis dania
+                <textarea placeholder='Wpisz dokładny opis dania'/>
+                </label>
+            </form>
+        </div>
     )
 }
 
