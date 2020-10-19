f56384f69c6d52e6e4023113b5636ad7666dcce6
abazurek <skala2294@gmail.com>
2020-10-19 14:40:53 +0200

    done validate name in add recipe form

--- src/js/AddRecipe.js
+++ src/js/AddRecipe.js
@@ -4,21 +4,41 @@ import {useHistory} from "react-router-dom";
 
 const newRecipe = {
     "name": "",
-    "ingredients": "",
     "shortDesc": "",
+    "need":"",
+    "ingredients": "",
     "longDesc": ""
 
+};
+
+const style={
+    color:"red",
+    fontSize:"18px"
 };
 
 function AddRecipe(name) {
     let history = useHistory();
 
-
+    const [recipe, setRecipe]=useState(newRecipe);
+    const [message, setMessage]=useState(newRecipe);
     function submitForm(e) {
         e.preventDefault();
+        if(recipe.name.length<3 ){
+            setMessage(prev=>({...prev, name:"Nazwa dania nie moźe być krótsza niż 3 znaki"}));
+            return;
+        } else  setMessage(prev=>({...prev, name:""}));
+
+
         history.push(`/account/${name}`)
 
     }
+
+    // function returnError(elem) {
+    //     const mess=message;
+    //     const info={elem}
+    //     return  <span style={style}>{mess.elem}</span>
+    //
+    // }
 
     return (
         <div className="container my-recipe">
@@ -27,7 +47,8 @@ function AddRecipe(name) {
                 <label>
                     {" "}
                     <span>Nazwa dania</span>
-                    <input type="text" placeholder="Wpisz nazwę dania"/>
+                    <input type="text" placeholder="Wpisz nazwę dania" onChange={({target})=>setRecipe(prev=>({...prev, name:target.value}))}/>
+                    <span style={style}>{message.name}</span>
                 </label>
                 <label>
                     {" "}
@@ -36,11 +57,13 @@ function AddRecipe(name) {
                         type="text"
                         placeholder="Wpisz potrzebne składniki po porzecinkach np. jajka, mleko"
                     />
+                    <span style={style}>{message.need}</span>
                 </label>
                 <label>
                     {" "}
                     <span>Krótki opis dania</span>
                     <textarea placeholder="Wpisz krótki opis dania"/>
+                    <span style={style}>{message.shortDesc}</span>
                 </label>
                 <label>
                     {" "}
@@ -49,11 +72,13 @@ function AddRecipe(name) {
                         type="text"
                         placeholder="Wpisz składniki po porzecinkach np. 4 jajka, pół litra mleka"
                     />
+                    <span style={style}>{message.ingredients}</span>
                 </label>
                 <label>
                     {" "}
                     <span> Dokładny opis opis dania</span>
                     <textarea placeholder="Wpisz dokładny opis dania"/>
+                    <span style={style}>{message.longDesc}</span>
                 </label>
                 <button type="submit">Dodaj przepis</button>
             </form>
