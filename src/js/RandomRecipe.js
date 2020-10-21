0ffb57bdbc03754796acc85250ecfcb75c2e847f
abazurek <skala2294@gmail.com>
2020-10-21 13:23:36 +0200

    often refreshing my own recipes section but not always, no idea to cause of this problem

--- src/js/RandomRecipe.js
+++ src/js/RandomRecipe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-function RandomRecipe({update,info,name,recipes}) {
+function RandomRecipe({updateFav,info,name,recipes}) {
 
     const currentDayNumber = require('current-day-number');
     const indexOfRecipe = currentDayNumber() % (recipes.length);
@@ -10,7 +10,7 @@ function RandomRecipe({update,info,name,recipes}) {
         <div className='for-today container'>
             <span>Przepis na dziś:</span>
             {recipes ? <div className='recipe-for-today'>
-                <Recipe update={update} info={info} name={name} recipe={recipes[indexOfRecipe]}/>
+                <Recipe updateFav={updateFav} info={info} name={name} recipe={recipes[indexOfRecipe]}/>
             </div> : <span className='loading container'>Loading...</span>}
         </div>
     )
